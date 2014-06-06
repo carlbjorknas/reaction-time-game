17c60012b1b83d06fc88f89edbce3b29f592b76d
carlbjorknas <carlbjorknas@localhost>
2014-06-06 23:11:22 +0200

    Nothing working yet. First plan is to collect player names and send the list of player name to all clients.

---
+++ ReactionGame/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactionGame.MvcApplication" Language="C#" %>
---
+++ ReactionGame/Hubs/ReactionGameHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ReactionGame.Hubs
+{
+    public class ReactionGameHub : Hub
+    {
+        private List<string> players = new List<string>();
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Join(string name)
+        {
+            players.Add(name);
+            Clients.All.updateListOfPlayers(players);
+        }
+    }
+}
