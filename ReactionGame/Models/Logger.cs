3c0f78a716255c020f3c64db2fac6d7d95fb2edf
carlbjorknas <carlbjorknas@localhost>
2014-06-15 22:21:11 +0200

    Added styling. Avoided logging bug where server variable was null.

--- ReactionGame/Models/Logger.cs
+++ ReactionGame/Models/Logger.cs
@@ -9,12 +9,18 @@ namespace ReactionGame.Models
     public class Logger
     {
         private object _lock = new object();
+        private string _path;
+
+        public Logger()
+        {
+            _path = HttpContext.Current.Server.MapPath("~/log.txt");
+        }
         public void Log(string message)
         {
             var logMessage = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "   " + message;
             lock (_lock)
             {                
-                File.AppendAllLines(HttpContext.Current.Server.MapPath("~/log.txt"), new[] { logMessage });
+                File.AppendAllLines(_path, new[] { logMessage });
             }
         }
     }
